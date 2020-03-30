386e8a0f88896a594360f1563eeee1cf762226c5
Dan Foley <dan@dan-foley.com>
2020-03-30 17:43:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-  end
+
 
   if req.path.match(/items/)
     resp.write "Item page!"
   end
+end
 
 end
