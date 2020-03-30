181d60f69989f1b7f55a6181537cdc1c805c0397
Dan Foley <dan@dan-foley.com>
2020-03-30 17:52:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,8 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/items/").last
       if @@items.include?(item)
+        
+      end
     else
       resp.write "Route not found"
       resp.status = 404
